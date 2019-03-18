638f739c11151f84bd6f248d49f87bc7cb5a113c
Sharath George M <sharathgeorgem@gmail.com>
2019-03-18 19:00:02 +0530

    Implemented Vehicles component. Added styling.

--- src/components/Vehicles/Vehicles.js
+++ src/components/Vehicles/Vehicles.js
@@ -9,6 +9,8 @@ class Vehicles extends Component {
       selectedPlanets: this.props.location.state,
       vehicles: []
     }
+    this.vehicleToFileName = this.vehicleToFileName.bind(this)
+    this.handleVehicleClick = this.handleVehicleClick.bind(this)
   }
   componentDidMount () {
     axios.get('https://findfalcone.herokuapp.com/vehicles')
@@ -20,11 +22,63 @@ class Vehicles extends Component {
       })
       .catch(error => console.log(error))
   }
+  vehicleToFileName (vehicle) {
+    let vehicleName = vehicle.name.split('')
+    vehicleName.splice(vehicleName.indexOf(' '), 1, '_')
+    return vehicleName.join('')
+  }
+  handleVehicleClick (index, e) {
+    let vehicles = this.state.vehicles
+    console.log('The click event contains ', e)
+    if (vehicles[index].total_no > 0) {
+      --vehicles[index].total_no
+      this.setState({vehicles})
+      console.log('The total number is given as ', this.state.vehicles[index].total_no)
+    } else {
+      console.log('You just exhausted your options. No more clicks.')
+    }
+  }
   render () {
     console.log(this.state.selectedPlanets)
     console.log(this.state.vehicles)
+    // let vehicles
+    // let vehiclesPath = 'https://res.cloudinary.com/dmmb5w7sm/image/upload/v1552882273/'
+    // if (this.state.vehicles.length) {
+    //   vehicles = this.state.vehicles.map((vehicle, index) => {
+    //     let vehicleName = this.vehicleToFileName(vehicle)
+    //     console.log('The mod vehicle name is ', vehicleName)
+    //     return <figure key={index}>
+    //       <img src={vehiclesPath + vehicleName + '.png'}
+    //         alt={vehicleName}
+    //         className='vehicle-image'
+    //         onClick={(e) => console.log('Vehicle click')} />
+    //       <figcaption>{vehicle.max_distance} Mmiles({vehicle.total_no})</figcaption>
+    //     </figure>
+    //   })
+    // }
+    let planets, vehicles
+    let planetPath = 'https://res.cloudinary.com/dmmb5w7sm/image/upload/v1552746276/'
+    if (this.state.vehicles.length) {
+      planets = this.state.selectedPlanets.map((planet, index) => {
+        return <img src={planetPath + planet.name.toLowerCase() + '.png'}
+          alt={planet.name}
+          className='selected-planets'
+          key={index}
+          onClick={(e) => console.log('Supta')} />
+      })
+      vehicles = this.state.vehicles.map((vehicle, index) => {
+        return <li key={index} onClick={(e) => this.handleVehicleClick(index, e)}>{vehicle.name} - {vehicle.total_no}</li>
+      })
+    }
     return (
-      <h1>Vehicles Component</h1>
+      <div className='vehicles'>
+        <h1 className='head'>Vehicle to deploy for each planet.</h1>
+        {planets}
+        <ul className='vehicle-list'>{vehicles}</ul>
+        <ul className='vehicle-list'>{vehicles}</ul>
+        <ul className='vehicle-list'>{vehicles}</ul>
+        <ul className='vehicle-list'>{vehicles}</ul>
+      </div>
     )
   }
 }
